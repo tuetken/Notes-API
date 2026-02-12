f7ed41b255be2370310a2a8ca811dc12f8b01cb1
Tyler <t.tuetken@outlook.com>
2026-02-12 17:00:21 -0500

    feat: added schema logic to note model

---
+++ models/Note.js
@@ -0,0 +1,24 @@
+import mongoose, { mongo } from "mongoose";
+
+const notesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100,
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 1000,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Note = mongoose.model("Note", notesSchema);
+
+export default Note;
